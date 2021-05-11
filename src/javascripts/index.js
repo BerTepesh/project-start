d2840ea1414b3d98b22298c5e48b2aa9b6f855ec
Alexey Berezin <alexey.berezin@videinfra.com>
2021-05-11 16:49:20 +0300

    ham panel

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,20 +1,9 @@
 import $ from 'jquery';
-// Expose jQuery
 window.$ = window.jQuery = $;
 
-$(function(){
-	if($(window).scrollTop() > 100) {
-		$(".header_sticky").addClass("active");
-	}
-	$(window).scroll(function() {
-		if($(window).scrollTop() < 1) {
-			$(".header_sticky").removeClass("sticky");
-			$(".header_sticky").removeClass("active");
-		} else if($(window).scrollTop() > 120){
-			$(".header_sticky").removeClass("sticky");
-			$(".header_sticky").addClass("active");
-		} else {
-			$(".header_sticky").addClass("sticky");
-		}
-	});
-});
+import './modules/sticky-header';
+import './modules/ham-panel';
+//import './util/polyfill/svg4everybody';
+
+
+
