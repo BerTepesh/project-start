7481776fc7a6ad0f69516944a61d87fbd42abd58
Alexey Berezin <alexey.berezin@videinfra.com>
2021-07-29 12:26:57 +0300

    preloader fix

--- src/javascripts/_entries.js
+++ src/javascripts/_entries.js
@@ -6,17 +6,18 @@ module.exports = {
     'landing':    [
         'common/main.js',
 
-        'modules/preloader.js',
+        
         'modules/lazy.js',
+        'modules/preloader.js',
     ],
 
     'test':    [
         'common/main.js',
         
         'modules/test/test-slider.js',
-        'modules/preloader.js',
         'modules/modal.js',
         'modules/lazy.js',
+        'modules/preloader.js',
     ],
 };
  
--- src/javascripts/modules/preloader.js
+++ src/javascripts/modules/preloader.js
@@ -1,12 +1,15 @@
 import $ from 'jquery';
 
-$(document).ready(function($) {
-	$('body').addClass('fixed');
-	$(window).on('load', function () {
-		var $preloader = $('.preloader'),
-		    $loader = $preloader.find('.preloader__loader');
-		$loader.fadeOut();
-		$preloader.delay(250).fadeOut(200);
-		$('body').removeClass('fixed');
+var loadData = new Promise(function (resolve) {
+	document.getElementsByTagName('body')[0].classList.add('fixed');
+	setTimeout(resolve, .1);
+});
+
+$(window).on('load', function () {
+	loadData.then(function () {
+		let preloaderEl = document.getElementById('preloader');
+		document.getElementsByTagName('body')[0].classList.remove('fixed');
+    preloaderEl.classList.add('hidden');
+    preloaderEl.classList.remove('visible');
 	});
 });
