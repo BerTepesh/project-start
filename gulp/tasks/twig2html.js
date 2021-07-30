971eb7f25b16998aeceee17d8381099af01f1257
Alexey Berezin <alexey.berezin@videinfra.com>
2021-07-30 18:33:23 +0300

    html pretty

--- gulp/tasks/twig2html.js
+++ gulp/tasks/twig2html.js
@@ -1,11 +1,15 @@
 const paths 				= require('../paths'),
 			gulp 					= require('gulp'),
 			plumber 			= require('gulp-plumber'),
+			htmlmin 			= require('gulp-html-minifier'),
+			prettyHtml 		= require('gulp-pretty-html'),
 			twig 					= require('gulp-twig');
 
 module.exports = function twig2html(cb) {
 	return gulp.src(paths.src.html)
 		.pipe(plumber())
 		.pipe(twig())
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(prettyHtml())
 		.pipe(gulp.dest(paths.build.html))
 }
