bdda2a8933eec45ced9f6830a59d3354fa1f1805
Alexey Berezin <alexey.berezin@videinfra.com>
2021-07-30 18:42:23 +0300

    html pretty fix

--- gulp/tasks/twig2html.js
+++ gulp/tasks/twig2html.js
@@ -9,7 +9,11 @@ module.exports = function twig2html(cb) {
 	return gulp.src(paths.src.html)
 		.pipe(plumber())
 		.pipe(twig())
-		.pipe(htmlmin({collapseWhitespace: true}))
-		.pipe(prettyHtml())
+		// .pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(prettyHtml({
+			indent_size: 2,
+			indent_char: ' ',
+			preserve_newlines: false
+		}))
 		.pipe(gulp.dest(paths.build.html))
 }
