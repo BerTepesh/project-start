dbe5f3a268336704df68f6d7d8f77ef1c1000990
Alexey Berezin <alexey.berezin@videinfra.com>
2021-08-02 11:00:25 +0300

    html-minifier delete

--- gulp/tasks/twig2html.js
+++ gulp/tasks/twig2html.js
@@ -1,7 +1,6 @@
 const paths 				= require('../paths'),
 			gulp 					= require('gulp'),
 			plumber 			= require('gulp-plumber'),
-			htmlmin 			= require('gulp-html-minifier'),
 			prettyHtml 		= require('gulp-pretty-html'),
 			twig 					= require('gulp-twig');
 
@@ -9,7 +8,6 @@ module.exports = function twig2html(cb) {
 	return gulp.src(paths.src.html)
 		.pipe(plumber())
 		.pipe(twig())
-		// .pipe(htmlmin({collapseWhitespace: true}))
 		.pipe(prettyHtml({
 			indent_size: 2,
 			indent_char: ' ',
